42077eb32d147c38f32ca4963b6d429eee04f169
JezerGonzalez <jezergonzalez@gmail.com>
2024-02-21 10:25:26 -0400

    printing numbers using putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- printing single digits from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+	}
+	putchar('\n');
+
+	return (0);
+}
