d52c4d3deea7d9873ceafc77841b0727efe2ff11
JezerGonzalez <jezergonzalez@gmail.com>
2024-02-21 11:04:17 -0400

    printing numbers and alphabet using ascii

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main- printing alphabet and numbers in ascii
+ *
+ * Return- Always 0 (Success)
+ */
+int main(void)
+{
+	int hexnum = 48;
+	int hexalpha = 97;
+
+	while (hexnum <= 57)
+	{
+		putchar(hexnum);
+		hexnum++;
+	}
+	while (hexalpha <= 102)
+	{
+		putchar(hexalpha);
+		hexalpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
