e8c7c591b24aebf0e9c34d20514357568413bc7d
JezerGonzalez <jezergonzalez@gmail.com>
2024-02-22 12:35:19 -0400

    printing alphabet 10 times using print_alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int newLine;
+
+	for (newLine = 0; newLine <= 10; newLine++)
+	{
+		print_alphabet();
+	}
+}
