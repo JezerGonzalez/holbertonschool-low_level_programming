14c7355a87e9206ce850714c229e35cace15f974
JezerGonzalez <jezergonzalez@gmail.com>
2024-02-26 13:04:29 -0400

    prints numbers 10x

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers 0 to 14 ten times
+ */
+void more_numbers(void)
+{
+	int num, newLine;
+
+	for (newLine = 0; newLine <= 10; newLine++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+		if (num >= 10)
+			_putchar((num / 10) + '0');
+		_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
