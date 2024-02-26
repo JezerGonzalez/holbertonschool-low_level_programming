278f20b85454767def87be62f59e8cdec47a26e0
JezerGonzalez <jezergonzalez@gmail.com>
2024-02-26 14:48:41 -0400

    Print '\' in a diagonal form

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - print '\' in a diagonal form
+ *
+ * @n: ammount of '\'
+ */
+void print_diagonal(int n)
+{
+	int line, space;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+		{
+			_putchar('\\');
+			_putchar('\n');
+			for (space = 0; space <= line; space++)
+				_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
