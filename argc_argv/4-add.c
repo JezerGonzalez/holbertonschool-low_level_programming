3f58c14b323ad6e924e3013a437258e6f7992a24
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-07 15:20:13 -0400

    sum of multiple numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ *main - prints the sum of 2 intergers
+ *@argc: argument count
+ *@argv: argument value
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+	int result = 0;
+	int j = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (count = 1; argv[count] != NULL ; count++)
+	{
+		for (j = 0; argv[count][j] != '\0'; j++)
+		{
+			if (isdigit(argv[count][j]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[count]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
