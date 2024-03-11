16a02aadd5983e4f8ddd39c757eea1bb81395957
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-11 13:15:28 -0400

    initialize variables

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- *
- *
- *
- *
+ * init_dog - initializes dog variables
+ *@d: pointer
+ *@name: name of dog
+ *@age: dog's age
+ *@owner: owner's name
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
