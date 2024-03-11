ba936df1f3f683a8c3a18aa1fbc3c68c71bcf968
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-11 11:37:49 -0400

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure to be called by the main
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
