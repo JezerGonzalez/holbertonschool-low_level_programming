8e7550592dba988268e1a8999dabfa0b20e7c4e8
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-12 13:11:14 -0400

    a funtion that creates a new dg

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,19 +6,21 @@
  *@dest: empty string
  * Return: Duplicated string
  */
-char *_strdup(char *dest)
+char *_strdup(char *src)
 {
 	int i = 0;
 	int length = 0;
-	char *ptr = NULL;
+	char *ptr;
 
-	for (length = 0; dest[length] != '\0'; length++)
+	if (src == NULL)
+		return (NULL);
+	for (length = 0; src[length] != '\0'; length++)
 	{}
 	ptr = malloc((length + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; dest[i] != '\0'; i++)
-		ptr[i] = dest[i];
+	for (i = 0; src[i] != '\0'; i++)
+		ptr[i] = src[i];
 	return (ptr);
 }
 
@@ -33,15 +35,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = NULL;
 
+	if(name == NULL || owner == NULL)
+		return (NULL);
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = _strdup(name);
 	new_dog->owner = _strdup(owner);
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
-		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
