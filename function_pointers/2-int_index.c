3134e4b47f4b41fcfaadafe1f461a13dfe926193
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-13 13:01:00 -0400

    finds the number

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ *int_index - iterates to find the number
+ *@array: array
+ *@size: size of array
+ *@cmp: function pointer
+ *Return: index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (*array == '\0')
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		(*cmp)(array[i]);
+		if ((*cmp)(array[i]) != 0)
+			break;
+	}
+	return (i);
+}
