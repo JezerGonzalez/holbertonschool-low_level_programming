f4f01c95c7fe0596da6188fc6d56a7edaf499c73
JezerGonzalez <jezergonzalez@gmail.com>
2024-03-15 12:14:42 -0400

    print all strings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(args, char *);
-		if (*string == '\0')
+		if (string == NULL)
 			printf("(nil)");
 		else
 			printf("%s", string);
