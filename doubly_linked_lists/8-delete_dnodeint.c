63ebee066567512cf8d538760fedc36528367217
JezerGonzalez <jezergonzalez@gmail.com>
2024-04-05 10:46:20 -0400

    deletes node at the nth index

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,18 +1,5 @@
 #include "lists.h"
 /**
- * dlistint_length - checks the lemgth of total nodes in the list
- *@head: start of node list
- *Return: totla length of nodes in a list
- */
-unsigned int dlistint_length(const dlistint_t *head)
-{
-	unsigned int i;
-
-	for (i = 0; head != NULL; i++, head = head->next)
-		;
-	return (i);
-}
-/**
  *delete_dnodeint_at_index - deetes node at the index of the list
  *@head: start of the list
  *@index: index to be deleted
@@ -21,28 +8,24 @@ unsigned int dlistint_length(const dlistint_t *head)
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp = *head;
-	unsigned int length;
 
-	length = dlistint_length(*head);
-	if (index > length)
-		return (-1);
 	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
 		if (temp->next != NULL)
 			temp->next->prev = NULL;
-		(*head) = (*head)->next;
+		*head = temp->next;
 		free(temp);
 		return (1);
 	}
 	for (; index != 1; --index, temp = temp->next)
-	{}
-	if (temp == NULL)
-		return (-1);
+	{
+		if (temp == NULL)
+			return (-1);
+	}
 	temp->prev->next = temp->next;
-	if (temp->next != NULL)
-		temp->next->prev = temp->prev;
+	temp->next->prev = temp->prev;
 	free(temp);
 	return (1);
 }
