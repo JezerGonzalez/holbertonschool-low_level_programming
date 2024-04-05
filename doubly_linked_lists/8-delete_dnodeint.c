86a5297e9aa3fa262e7c70c76cba6fc24e12d98c
JezerGonzalez <jezergonzalez@gmail.com>
2024-04-05 02:17:13 -0400

    deletes node at the nth index

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ *delete_dnodeint_at_index - deetes node at the index of the list
+ *@head: start of the list
+ *@index: index to be deleted
+ *Return: 1 if successful or -1 if failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		if (temp->next != NULL)
+			temp->next->prev = NULL;
+		(*head) = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	for (; index != 1; --index, temp = temp->next)
+	{}
+	if (temp == NULL)
+		return (-1);
+	temp->prev->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
+	free(temp);
+	return (1);
+}
