544cd7c1e899e1be8ddf88caa1aa36ea103ebcb0
JezerGonzalez <jezergonzalez@gmail.com>
2024-04-06 15:01:37 -0400

    reads a file to print it out

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,7 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == 0 || letters == 0)
 		return (0);
-	buffer = malloc(sizeof(char *) * letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 	file = open(filename, O_RDONLY);
