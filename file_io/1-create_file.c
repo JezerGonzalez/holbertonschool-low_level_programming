74674c5da22139190bb7bca582b3af9d5a786511
JezerGonzalez <jezergonzalez@gmail.com>
2024-04-07 05:04:49 -0400

    creating files

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,13 +12,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 	new_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (new_file == -1)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
 	write(new_file, text_content, i);
 	close(new_file);
 	return (1);
