0678f625101c716f293bc1e3f9aa052cf8e78c15
JezerGonzalez <jezergonzalez@gmail.com>
2024-04-07 17:04:52 -0400

    append to file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -11,17 +11,17 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	existing_file = open(filename, O_WRONLY | O_APPEND);
+	if (existing_file == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
 			;
+		new_text = write(existing_file, text_content, len);
+		if (new_text == -1)
+			return (-1);
 	}
-	existing_file = open(filename, O_WRONLY | O_APPEND);
-	if (existing_file == -1)
-		return (-1);
-	new_text = write(existing_file, text_content, len);
-	if (new_text == -1)
-		return (-1);
 	if (new_text != len)
 	{
 		close(existing_file);
